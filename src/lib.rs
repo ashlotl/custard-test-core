4b6a9f9e4abcf8381ac9289aa1c71c63a6bfbd5e
Ashlotl <thesupremeliverwurst@gmail.com>
2022-02-27 19:26:17 -0800

    Got datachunk loading working

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,5 @@
-use custard_use::{dylib_management::safe_library::core_crate::CompositionFunctionReturn, utils::files::get_maybe_const_string};
+use custard_macros::{attach_composition, attach_unloaded_datachunk_getter};
 
-#[no_mangle]
-pub fn composition() -> CompositionFunctionReturn {
-	let ret = get_maybe_const_string(concat!(env!("CARGO_MANIFEST_DIR"), "/src/composition.ron"), include_str!("composition.ron"));
-	println!("fetched composition with status {}: {}", ret.1, ret.0);
-	ret.0
-}
+attach_composition!();
+
+attach_unloaded_datachunk_getter!();
