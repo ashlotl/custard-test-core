8c27a684d96a18a6ce1caa8dc5d19c042db25f10
Ashlotl <thesupremeliverwurst@gmail.com>
2022-03-06 01:17:16 -0800

    got basic task functionality working

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use custard_macros::{attach_composition, attach_unloaded_datachunk_getter};
+use custard_macros::{attach_composition, attach_unloaded_datachunk_getter, attach_unloaded_task_getter};
 
 attach_composition!();
 
 attach_unloaded_datachunk_getter!();
+
+attach_unloaded_task_getter!();
